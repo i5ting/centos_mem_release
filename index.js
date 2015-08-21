222335dc6d4f2718ef99cefaf87a3517e3478129
i5ting <i5ting@126.com>
2015-08-21 08:45:59 +0800

    init

--- index.js
+++ index.js
@@ -15,19 +15,9 @@ if ( argv.length > 1 ) {
   }
 }
 
-// // execFile: executes a file with the specified arguments
-// child_process.exec('cat *.js bad_file | wc -l',
-//   function (error, stdout, stderr) {
-//     console.log('stdout: ' + stdout);
-//     console.log('stderr: ' + stderr);
-//     if (error !== null) {
-//       console.log('exec error: ' + error);
-//     }
-// });
-
-execFile: executes a file with the specified arguments
+// execFile: executes a file with the specified arguments
 child_process.execFile(file_path + '/do.sh',null,{cwd:current_path},function (error,stdout,stderr) {
     if (error !== null) {
-      console.log('exec error: ' + error);
+      console.log('it is only run on centos. exec error: ' + error);
     }
 });
